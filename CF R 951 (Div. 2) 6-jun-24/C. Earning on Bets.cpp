ed57d734c7a22d25aa38fd92857167db4d590fd8
Jesusrac31 <jesusraceros@gmail.com>
2024-06-23 18:16:46 +0200

    23-6-2024-18:16

---
+++ CF R 951 (Div. 2) 6-jun-24/C. Earning on Bets.cpp
@@ -0,0 +1,190 @@
+//Librerías incluidas en #include<bitstdc++.h>
+#include<algorithm>
+#include<array>
+#include<atomic>
+#include<bitset>
+#include<ccomplex>
+#include<cerrno>
+#include<cfenv>
+#include<cfloat>
+#include<chrono>
+#include<cinttypes>
+#include<ciso646>
+#include<climits>
+#include<clocale>
+#include<cmath>
+#include<complex>
+#include<condition_variable>
+#include<csetjmp>
+#include<csignal>
+#include<cstdalign>
+#include<cstdarg>
+#include<cstdbool>
+#include<cstddef>
+#include<cstdint>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<ctgmath>
+#include<ctime>
+#include<cwchar>
+#include<cwctype>
+#include<deque>
+#include<exception>
+#include<forward_list>
+#include<fstream>
+#include<functional>
+#include<future>
+#include<initializer_list>
+#include<iomanip>
+#include<ios>
+#include<iosfwd>
+#include<iostream>
+#include<istream>
+#include<iterator>
+#include<limits>
+#include<list>
+#include<locale>
+#include<map>
+#include<memory>
+#include<mutex>
+#include<new>
+#include<numeric>
+#include<ostream>
+#include<queue>
+#include<random>
+#include<ratio>
+#include<regex>
+#include<scoped_allocator>
+#include<set>
+#include<sstream>
+#include<stack>
+#include<stdexcept>
+#include<streambuf>
+#include<string>
+#include<system_error>
+#include<thread>
+#include<tuple>
+#include<type_traits>
+#include<typeindex>
+#include<typeinfo>
+#include<unordered_map>
+#include<unordered_set>
+#include<utility>
+#include<valarray>
+#include<vector>
+#include <stdlib.h>
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<long long int> vll;
+typedef long long int lli;
+typedef pair<int, int> pii;
+typedef map<string, int> msi;
+typedef map<int, vector<int>> miv;
+
+// Funciones vector
+#define PB(a) push_back(a);
+
+bool sort_func(int a, int b) {
+  if (a < b) {
+    return true;
+  } else {
+    return false;
+  }
+}
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
+
+#define borra_el(vect, el) vect.erase(vect.find(el));
+#define borra_range(vect, a, b) vect.erase(a, b);
+#define borra(vect, n) vect.erase(vect.begin() + n);
+#define B begin();
+#define E end();
+#define copia(v1, v2)                                                          \
+  ;                                                                            \
+  copy(v1.begin(), v1.end(), back_inserter(v2));
+
+// Funciones map
+#define F first;
+#define S second;
+
+// Logaritmo de 2
+double log_2 = log(2);
+double log2(int a) { return (log(a) / log_2); }
+
+void Imprime(vll vect) {
+  for (int i = 0; i < vect.size(); i++) {
+    cout << vect[i] << " ";
+  }
+  cout << "\n";
+}
+
+void Imprime_set(set<int> s) {
+  copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+  cout << endl;
+}
+
+lli maximo_comun_divisor(lli a, lli b) {
+  lli temporal; // Para no perder b
+  while (b != 0) {
+    temporal = b;
+    b = a % b;
+    a = temporal;
+  }
+  return a;
+}
+
+lli minimo_comun_multiplo(lli a, lli b) {
+  return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+  auto it = str.begin();
+  while (it != str.end() && isdigit(*it)) {
+    it++;
+  }
+  return !str.empty() && it == str.end();
+}
+
+vll lee(int n) {
+  lli el;
+  vll vect;
+  for (int i = 0; i < n; i++) {
+    cin >> el;
+    vect.PB(el);
+  }
+  return (vect);
+}
+
+int solve() {
+  // Code aquí
+  int n;
+  cin >> n;
+  vll v, c;
+  v = lee(n);
+  lli sol=v[0], monedas=0;
+  for (int i = 1; i<n; i++){
+    sol = minimo_comun_multiplo(sol, v[i]);
+  }
+  for (int i = 0; i<n; i++){
+    c.PB(sol/v[i]);
+    monedas+=sol/v[i];
+  }
+  if (monedas < sol){
+    Imprime(c);
+  } else {
+    cout << -1 << endl;
+  }
+
+  return 0;
+}
+
+int main() {
+  int T;
+  cin >> T; // Número de casos
+  while (T--) {
+    solve();
+  }
+  return 0;
+}
+

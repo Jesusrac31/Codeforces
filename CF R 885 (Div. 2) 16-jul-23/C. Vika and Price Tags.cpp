a685003734a72681806a99dbfe6406e17cc70cc3
Jesusrac31 <jesusraceros@gmail.com>
2026-04-03 10:45:51 +0200

    Problems[C]-Contest1848

--- CF R 885 (Div. 2) 16-jul-23/C. Vika and Price Tags.cpp
+++ CF R 885 (Div. 2) 16-jul-23/C. Vika and Price Tags.cpp
@@ -1,118 +1,209 @@
-#include <bits/stdc++.h>
-#include <iostream>
-#include <stdlib.h>
+#ifdef DEBUG
+#define _GLIBCXX_DEBUG
+#endif
+
+#include<bits/stdc++.h>
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("O3,unroll-loops")
+//#pragma GCC target("avx2")
+
+#ifdef DEBUG
+#define DBG_COUT(stmt) do { stmt; } while (0)
+#else
+#define DBG_COUT(stmt) do {} while (0)
+#endif
+
 using namespace std;
 
 typedef vector<int> vi;
-typedef vector<long long int> vll;
-typedef long long int lli;
+typedef vector<long long> vll;
+typedef long long lli;
 typedef pair<int, int> pii;
 typedef map<string, int> msi;
 typedef map<int, vector<int>> miv;
 
+const int MOD = 998244353; // Módulo del problema, cambiar en caso de no ser ese. NO TIENE PORQUÉ SER CONSTANTE, SOLO GLOBAL
+
+struct Mint { // Es una estructura como el int pero que trabaja en mod MOD
+    int v;
+    Mint(long long val = 0) {
+        v = int(val % MOD);
+        if (v < 0) v += MOD;
+    }
+    Mint operator+(const Mint &o) const { return Mint(v + o.v); }
+    Mint operator-(const Mint &o) const { return Mint(v - o.v); }
+    Mint operator*(const Mint &o) const { return Mint(1LL * v * o.v); }
+    Mint operator/(const Mint &o) const { return *this * o.inv(); }
+    Mint& operator+=(const Mint &o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    Mint& operator-=(const Mint &o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    Mint& operator*=(const Mint &o) { v = int(1LL * v * o.v % MOD); return *this; }
+    bool operator<(const Mint& o) const {return v < o.v;}
+    bool operator>(const Mint& o) const {return v > o.v;}
+    bool operator<=(const Mint& o) const {return v <= o.v;}
+    bool operator>=(const Mint& o) const {return v >= o.v;}
+    bool operator==(const Mint& o) const {return v == o.v;}
+    bool operator!=(const Mint& o) const {return v != o.v;}
+    Mint pow(long long p) const {
+        Mint a = *this, res = 1;
+        while (p > 0) {
+            if (p & 1) res *= a;
+            a *= a;
+            p >>= 1;
+        }
+        return res;
+    }
+    Mint inv() const { return pow(MOD - 2); }
+    friend ostream& operator<<(ostream& os, const Mint& m) {
+        os << m.v;
+        return os;
+    }
+};
+istream& operator>>(std::istream& input, Mint& m) {
+    input >> m.v;
+    return input;
+}
+
 // Funciones vector
 #define PB(a) push_back(a);
 
-bool sort_func(pii a, pii b) {
-  if (a.first < b.first) {
-    return true;
-  } else {
-    return false;
-  }
+bool sort_func(int a, int b) {
+    if (a < b) {
+        return true;
+    } else {
+        return false;
+    }
 }
-#define ord(vect) sort(vect.begin(), vect.end(), sort_func);
-
+#define ord(vect) sort(vect.begin(), vect.end(), sort_func)
+#define rep(x,n) for(int x = 0; x < n; ++x)
 #define borra_el(vect, el) vect.erase(vect.find(el));
 #define borra_range(vect, a, b) vect.erase(a, b);
 #define borra(vect, n) vect.erase(vect.begin() + n);
 #define B begin();
 #define E end();
-#define copia(v1, v2)                                                          \
-  ;                                                                            \
-  copy(v1.begin(), v1.end(), back_inserter(v2));
+#define copia(v1, v2)                                                                                                                    \
+    ;                                                                                                                                                        \
+    copy(v1.begin(), v1.end(), back_inserter(v2));
 
-// Funciones map
-#define F first;
-#define S second;
+// Funciones pair
+#define F first
+#define S second
 
 // Logaritmo de 2
 double log_2 = log(2);
 double log2(int a) { return (log(a) / log_2); }
 
-void Imprime(vi vect) {
-  for (int i = 0; i < vect.size(); i++) {
-    cout << vect[i] << " ";
-  }
-  cout << endl;
+// Imprime cualquier vector 
+template<typename T> std::ostream& operator<<(std::ostream& os, const std::vector<T>& vec) {
+    os << "[ ";//Quita esto si no quieres los corchetes o cambia lo que quieras poner
+    for(const auto& elem : vec) {
+        os << elem << " ";
+    }
+    os << "]";
+    return os;
 }
 
-vi lee(int n) {
-  int el;
-  vi vect;
-  for (int i = 0; i < n; i++) {
-    cin >> el;
-    vect.PB(el);
-  }
-  return (vect);
+void Imprime_set(set<int> s) {
+    copy(s.begin(), s.end(), ostream_iterator<int>(cout, " "));
+    cout << endl;
+}
+
+int maximo_comun_divisor(int a, int b) {
+    int temporal; // Para no perder b
+    while (b != 0) {
+        temporal = b;
+        b = a % b;
+        a = temporal;
+    }
+    return a;
+}
+
+int minimo_comun_multiplo(int a, int b) {
+    return (a * b) / maximo_comun_divisor(a, b);
+}
+
+bool isNumeric(string const &str) {
+    auto it = str.begin();
+    while (it != str.end() && isdigit(*it)) {
+        it++;
+    }
+    return !str.empty() && it == str.end();
+}
+
+void lee(int n, vi& vect) {
+    rep(i, n) cin >> vect[i];
+    return ;
+}
+
+#define INF INT_MAX
+double pi = 2*acos(0.0);
+
+int getT(int a, int b){
+    if (a == 0) return 0;
+    if (b == 0) return 1;
+    int ops = 0;
+    int r = a%b;
+    int c = a/b;
+    int extra; // Actualiza el número de operaciones en este caso
+    int newA;
+    int newB;
+    if (r == 0) { 
+        DBG_COUT(cout << "Returing last: " << (2+((c+1)&1) + (((c+1)/2)*3)-3) << endl);
+        extra = 2+((c+1)&1);
+        return (((c+1)/2)*3)-3+extra;
+    } else if (c == 0) {
+        DBG_COUT(cout << "Ops, bigger\n");
+        newA = b-a;
+        newB = a;
+        extra = 2;
+    } else {
+        extra = 2*((c+1)&1)+1;
+        extra += ((c/2)*3);
+        if (c&1){
+            newA = b;
+            newB = r;
+        } else {
+            newA = r;
+            newB = b;
+        }
+    }
+    DBG_COUT(cout << "Op with " << a << " " << b << " Extra: " << extra << " newA and newB: " << newA << " " << newB << endl);
+    ops = extra + getT(newA, newB); // Haz la siguiente etapa
+    return ops;
 }
 
 int solve() {
-  // Code aquí
-  int n;
-  vi a, b;
-  cin >> n;
-  a = lee(n);
-  b = lee(n);
-
-  int tipo = 0, tipo_caso;
-  vector<pii> posibles(3);
-
-  for (int i = 0; i < n; i++) {
-    if (a[i] != 0 || b[i] != 0) {
-      posibles[0].first = a[i];
-      posibles[0].second = 1;
-      posibles[0].first = b[i];
-      posibles[0].second = 2;
-      posibles[0].first = abs(a[i] - b[i]);
-      posibles[0].second = 3;
-      if (posibles[0].first = a[i] == 0) {
-        tipo_caso = 1;
-      } else if (posibles[0].first = a[i] == 0) {
-        tipo_caso = 2;
-
-      } else if (posibles[0].first = a[i] == 0) {
-        tipo_caso = 3;
-
-      } else {
-        ord(posibles);
-        if (posibles[0] == posibles[1]) {
-          tipo_caso = posibles[2].second;
-        } else {
-          tipo_caso = posibles[1].second;
-        }
-      }
-      cout << tipo_caso << endl;
-      if (tipo == 0) {
-        tipo = tipo_caso;
-      } else {
-        if (tipo != tipo_caso) {
-          cout << "NO" << endl;
-          return 0;
-        }
-      }
+    // Code aquí
+    int n; cin >> n;
+    vi a(n), b(n); rep(i, n) cin >> a[i]; rep(i, n) cin >> b[i];
+    DBG_COUT(cout << "Test: " << n << "\n" << a << endl << b << endl);
+    int sol = -1;
+    int tAct;
+    rep(i,n){
+        DBG_COUT(cout << "Cycle " << i << " Values: " << a[i] << " " << b[i] << endl);
+    
+        if (a[i] == 0 && b[i] == 0) continue;
+        else if (a[i] == 0 && b[i] != 0) tAct = 0;
+        else if (a[i] != 0 && b[i] == 0) tAct = 1;
+        else {DBG_COUT(cout << "Starting Calculations:\n"); tAct = getT(a[i], b[i]) % 3;}
+        DBG_COUT(cout << "Result :" << tAct << endl);
+        if (sol == -1 || sol == tAct) sol = tAct;
+        else {cout << "NO" << endl; return 0;}
     }
-  }
-  cout << "YES" << endl;
+    cout << "YES" << endl;
 
-  return 0;
+    return 0;
 }
 
-int main() {
-  int T;
-  cin >> T; // Número de casos
-  while (T--) {
-    solve();
-  }
-  return 0;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr); 
+    int T;
+    cin >> T; // Número de casos
+    while (T--) {
+        solve();
+    }
+    return 0;
 }
-//Eliminar comentario si el proyecto esta terminado (Dinamica empezo el 21/06/2024)
+
+// https://codeforces.com/contest/1848/problem/C
--- README.md
+++ README.md
@@ -7,9 +7,9 @@ Además, hay una carpeta de problemas variados (`Randoms`).
 ## Resumen
 
 - Problemas intentados: **251**
-- Problemas resueltos: **226**
-- Problemas sin terminar: **25**
-- Porcentaje de resolución: **90.04%**
+- Problemas resueltos: **227**
+- Problemas sin terminar: **24**
+- Porcentaje de resolución: **90.44%**
 
 ## Criterio de estado
 
@@ -151,14 +151,6 @@ g++ -DDEBUG -std=c++17 -O2 -Wall -o main.exe "test.cpp" && .\main.exe < input.in
 - [A. Dora's Set.cpp](CF%20R%20969%20(Div.%202)%2030-ago-24/A.%20Dora's%20Set.cpp)
 - [B. Index and Maximum Value.cpp](CF%20R%20969%20(Div.%202)%2030-ago-24/B.%20Index%20and%20Maximum%20Value.cpp) (sin terminar)
 
-### Educational CF R 168 (Rated for Div. 2) 30-jul-24
-
-- [A. Strong Password.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/A.%20Strong%20Password.cpp)
-- [B. Make Three Regions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/B.%20Make%20Three%20Regions.cpp)
-- [C. Even Positions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/C.%20Even%20Positions.cpp)
-- [D. Maximize the Root.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/D.%20Maximize%20the%20Root.cpp)
-- [E. Level Up.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/E.%20Level%20Up.cpp) (sin terminar)
-
 ### Pinely R 4 (Div. 1 + Div. 2) 28-jul-24
 
 - [A. Maximize the Last Element.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/A.%20Maximize%20the%20Last%20Element.cpp)
@@ -166,6 +158,14 @@ g++ -DDEBUG -std=c++17 -O2 -Wall -o main.exe "test.cpp" && .\main.exe < input.in
 - [C. Absolute Zero.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/C.%20Absolute%20Zero.cpp)
 - [D. Prime XOR Coloring.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/D.%20Prime%20XOR%20Coloring.cpp)
 - [E. Coloring Game.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/E.%20Coloring%20Game.cpp)
+
+### Educational CF R 168 (Rated for Div. 2) 30-jul-24
+
+- [A. Strong Password.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/A.%20Strong%20Password.cpp)
+- [B. Make Three Regions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/B.%20Make%20Three%20Regions.cpp)
+- [C. Even Positions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/C.%20Even%20Positions.cpp)
+- [D. Maximize the Root.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/D.%20Maximize%20the%20Root.cpp)
+- [E. Level Up.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/E.%20Level%20Up.cpp) (sin terminar)
 
 ### CF R 962 (Div. 3) 26-jul-24
 
@@ -351,7 +351,7 @@ g++ -DDEBUG -std=c++17 -O2 -Wall -o main.exe "test.cpp" && .\main.exe < input.in
 
 - [A. Vika and Her Friends.cpp](CF%20R%20885%20(Div.%202)%2016-jul-23/A.%20Vika%20and%20Her%20Friends.cpp)
 - [B. Vika and the Bridge.cpp](CF%20R%20885%20(Div.%202)%2016-jul-23/B.%20Vika%20and%20the%20Bridge.cpp)
-- [C. Vika and Price Tags.cpp](CF%20R%20885%20(Div.%202)%2016-jul-23/C.%20Vika%20and%20Price%20Tags.cpp) (sin terminar)
+- [C. Vika and Price Tags.cpp](CF%20R%20885%20(Div.%202)%2016-jul-23/C.%20Vika%20and%20Price%20Tags.cpp)
 
 ### CF R 884 (Div. 1 + Div. 2) 11-jul-23
 

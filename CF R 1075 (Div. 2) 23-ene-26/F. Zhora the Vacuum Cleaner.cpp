ef0d13ec3bd3a701af9266365ab3119c84b94ea5
Jesusrac31 <jesusraceros@gmail.com>
2026-06-05 13:24:13 +0200

    Problems[F]-Contest2189

--- CF R 1075 (Div. 2) 23-ene-26/F. Zhora the Vacuum Cleaner.cpp
+++ CF R 1075 (Div. 2) 23-ene-26/F. Zhora the Vacuum Cleaner.cpp
@@ -8,12 +8,10 @@
 //#pragma GCC target("avx2")
 
 #ifdef DEBUG
-#include "lib/debug.h"
+#define DBG_COUT(stmt) do { stmt; } while (0)
 #else
-#define debug(...) 228
+#define DBG_COUT(stmt) do {} while (0)
 #endif
-
-#include<bits/stdc++.h>
 
 using namespace std;
 
@@ -132,11 +130,13 @@ bool isNumeric(string const &str) {
 }
 
 void lee(int n, vi& vect) {
-  rep(i, n) cin >> vect[i];
-  return ;
+    rep(i, n) cin >> vect[i];
+    return ;
 }
 
 #define INF INT_MAX
+double pi = 2*acos(0.0);
+
 
 struct Node {
     int id;
@@ -150,25 +150,137 @@ istream& operator>>(std::istream& input, Node& m) {
     return input;
 }
 
+vll a; // Valor de cada nodo (número de nueces)
+vector<vi> g; // Grafo, estructura del árbol con lista de adyascencia
+vll sub; // Cuantas nueces hay en el subarbol dado por enraizar el árbol en 1 y seleccionar el subárbol de i
+vi parent; // Cual es el padre de cada nodo si enraizamos el árbol en 1
+vi dead; // Guarda los nodos que están muertos
+
+lli total_nuts;
+
+/*
+Recorre todo el árbol con dfs, si un nodo tiene valor 0 y no tiene ramas con nodos vivos, este se considera muerto
+*/
+int dfs_dead(int u, int p) {
+    int ramas = (a[u] > 0);
+
+    for (int v : g[u]) {
+        if (v == p) continue;
+        ramas += dfs_dead(v, u);
+    }
+
+    if (a[u] == 0 && ramas <= 1) {
+        dead[u] = 1;
+    }
+
+    return ramas > 0;
+}
+
+// Calcula los padres de cada nodo y la suma de cada subárbol si enraizamos el árbol en 1
+void dfs_sum(int u, int p) {
+    parent[u] = p;
+    sub[u] = a[u];
+
+    for (int v : g[u]) {
+        if (v == p) continue;
+        dfs_sum(v, u);
+        sub[u] += sub[v];
+    }
+}
+
+// Devuelve la suma de frutos en la componente que contiene a u 
+// al quitar la arista (u, v).
+lli component_sum(int u, int v) {
+    if (parent[u] == v) { // Si v era el padre de u en el arbol enraizado en 1, sub[u] contiene el resultado
+        return sub[u];
+    } else { // En caso contrario, son todas las nueces menos las que están en el subarbol de v (sub[v])
+        return total_nuts - sub[v];
+    }
+}
 
 int solve() {
-    // Code aquí
-    int n, p, q;
+    int n;
+    lli p, q;
     cin >> n >> p >> q;
-    vector<Node> a(n);
-    for (int i = 0; i<n;i++) {
+
+    // Limpia las listas globales
+    a.assign(n + 1, 0);
+    g.assign(n + 1, vi());
+    sub.assign(n + 1, 0);
+    parent.assign(n + 1, 0);
+    dead.assign(n + 1, 0);
+
+    total_nuts = 0;
+
+    int root = -1; // Sirve para calcular los nodos muertos, no tiene más uso en el problema, solo hace falta que sea un nodo con valor != 0
+    int initial_alive = 0; // Los nodos que no empiezan con a[i] = 0
+
+    // Guarda el número de nueves de cada nodo
+    for (int i = 1; i <= n; i++) {
         cin >> a[i];
-        a[i].id = i;
-    }
-    vector<vector<Node*>> tree;
-    int u, v;
-    for (int i = 0; i<n-1; i++){
-        cin >> u >> v;
-        tree[u].PB(&a[v]);
-        tree[v].PB(&a[u]);
+        total_nuts += a[i]; // Calcula cuantas nueves hay en total
+
+        if (a[i] > 0) { // Si el nodo tiene elementos se almacena como vivo
+            root = i;
+            initial_alive++;
+        }
     }
 
-    
+    // Estructura el árbol
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+
+        g[u].push_back(v);
+        g[v].push_back(u);
+    }
+
+    // Si no hay nueces, el coste es 0
+    if (total_nuts == 0) {
+        cout << 0 << '\n';
+        return 0;
+    }
+
+    // 1. Marcamos vértices muertos.
+    dfs_dead(root, 0);
+
+    // 2. Calculamos sumas de componentes. 
+    // Podemos enraizar en cualquier nodo, por ejemplo 1.
+    dfs_sum(1, 0);
+
+    // 3. cost[u] = mínimo número de operaciones para dejar u vacío.
+    vll cost;
+
+    for (int u = 1; u <= n; u++) {
+        if (dead[u]) { // Si el vertice ya está muerto, cuesta 0 dejarlo vacío
+            cost.push_back(0);
+            continue;
+        }
+        // Si no está muerto, buscamos la forma en la que antes se elimina
+        lli best = LLONG_MAX;
+        for (int v : g[u]) {
+            // Por cada arista u, v, digamos que haces una operación en un nodo x que pasa por dicha arista,
+            // ¿Cuánto tardas en matar el nodo u? La respuesta es las nueces del subarbol de u si enraizamos el árbol en v.
+            best = min(best, component_sum(u, v));
+        }
+        // Almacena el mejor valor
+        cost.push_back(best);
+    }
+
+    sort(cost.begin(), cost.end());
+
+    // Solución por defecto, no hacer operaciones
+    lli ans = 1LL * initial_alive * q;
+
+    // Si dejamos vacíos x vértices, quedan n - x vértices que comer.
+    // Necesitamos cost[x - 1] operaciones.
+    for (int x = 1; x < n; x++) {
+        lli operations = cost[x - 1];
+        lli candidate = operations * p + 1LL * (n - x) * q;
+        ans = min(ans, candidate);
+    }
+
+    cout << ans << '\n';
 
     return 0;
 }
@@ -177,12 +289,19 @@ signed main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr); 
+    auto start = chrono::high_resolution_clock::now();
     int T;
     cin >> T; // Número de casos
     while (T--) {
         solve();
     }
+    auto finish = chrono::high_resolution_clock::now();
+    DBG_COUT(
+        chrono::duration<double> elapsed = finish - start;
+        cout << "Tiempo de ejecucion: " << elapsed.count() << " segundos\n";
+        cerr << "Tiempo de ejecucion: " << elapsed.count() << " segundos\n";
+    );
     return 0;
 }
 
-//Eliminar comentario si el proyecto está terminado (Dinámica empezó el 21/06/2024)
+// https://codeforces.com/contest/2189/problem/F
--- README.md
+++ README.md
@@ -163,14 +163,6 @@ Measure-Command{Get-Content input.in | .\main.exe > output.out}
 - [A. Dora's Set.cpp](CF%20R%20969%20(Div.%202)%2030-ago-24/A.%20Dora's%20Set.cpp)
 - [B. Index and Maximum Value.cpp](CF%20R%20969%20(Div.%202)%2030-ago-24/B.%20Index%20and%20Maximum%20Value.cpp)
 
-### Educational CF R 168 (Rated for Div. 2) 30-jul-24
-
-- [A. Strong Password.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/A.%20Strong%20Password.cpp)
-- [B. Make Three Regions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/B.%20Make%20Three%20Regions.cpp)
-- [C. Even Positions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/C.%20Even%20Positions.cpp)
-- [D. Maximize the Root.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/D.%20Maximize%20the%20Root.cpp)
-- [E. Level Up.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/E.%20Level%20Up.cpp)
-
 ### Pinely R 4 (Div. 1 + Div. 2) 28-jul-24
 
 - [A. Maximize the Last Element.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/A.%20Maximize%20the%20Last%20Element.cpp)
@@ -178,6 +170,14 @@ Measure-Command{Get-Content input.in | .\main.exe > output.out}
 - [C. Absolute Zero.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/C.%20Absolute%20Zero.cpp)
 - [D. Prime XOR Coloring.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/D.%20Prime%20XOR%20Coloring.cpp)
 - [E. Coloring Game.cpp](Pinely%20R%204%20(Div.%201%20%2B%20Div.%202)%2028-jul-24/E.%20Coloring%20Game.cpp)
+
+### Educational CF R 168 (Rated for Div. 2) 30-jul-24
+
+- [A. Strong Password.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/A.%20Strong%20Password.cpp)
+- [B. Make Three Regions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/B.%20Make%20Three%20Regions.cpp)
+- [C. Even Positions.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/C.%20Even%20Positions.cpp)
+- [D. Maximize the Root.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/D.%20Maximize%20the%20Root.cpp)
+- [E. Level Up.cpp](Educational%20CF%20R%20168%20(Rated%20for%20Div.%202)%2030-jul-24/E.%20Level%20Up.cpp)
 
 ### CF R 962 (Div. 3) 26-jul-24
 
